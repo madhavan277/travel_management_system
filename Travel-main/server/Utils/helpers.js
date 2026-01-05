0b06cca2daba6b0e4e2de2f59d09c7ee48e66aea
madhavan277 <139436593+madhavan277@users.noreply.github.com>
2026-01-05 17:23:02 +0530

    Delete Travel-main directory

--- Travel-main/server/Utils/helpers.js
+++
@@ -1,8 +0,0 @@
-exports.generateOTP = () => {
-  var digits = "0123456789";
-  let OTP = "";
-  for (let i = 0; i < 4; i++) {
-    OTP += digits[Math.floor(Math.random() * 10)];
-  }
-  return OTP;
-};
